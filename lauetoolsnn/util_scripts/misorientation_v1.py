dcb2629004e7b93f7406af9190bd9b3b4717a64a
Ravi <115461198+BM32ESRF@users.noreply.github.com>
2022-11-08 21:12:54 +0100

    updates

--- lauetoolsnn/util_scripts/misorientation_v1.py
+++ lauetoolsnn/util_scripts/misorientation_v1.py
@@ -15,6 +15,75 @@ FOr now, only CUBIC!!
 from scipy.spatial.transform import Rotation
 import numpy as np
 
+def euler_as_matrix(angles):
+    """
+    Return the active rotation matrix
+    """
+    # NOTE:
+    #   It is not recommended to directly associated Euler angles with
+    #   other common transformation concept due to its unique passive
+    #   nature.
+    phi2, phi, phi1 = angles
+    c1, s1 = np.cos(phi1), np.sin(phi1)
+    c, s = np.cos(phi), np.sin(phi)
+    c2, s2 = np.cos(phi2), np.sin(phi2)
+    return np.array([
+                        [c1 * c2 - s1 * c * s2, -c1 * s2 - s1 * c * c2, s1 * s],
+                        [s1 * c2 + c1 * c * s2, -s1 * s2 + c1 * c * c2, -c1 * s],
+                        [s * s2, s * c2, c],
+                    ])
+
+def quats_as_eulers(qs):
+    """
+    Quaternion to Euler angles
+    """
+    # NOTE: assuming Bunge Euler angle (z->x->z)
+    #   w = cos(Phi/2) * cos(phi1/2 + phi2/2)
+    #   x = sin(Phi/2) * cos(phi1/2 - phi2/2)
+    #   y = sin(Phi/2) * sin(phi1/2 - phi2/2)
+    #   z = cos(Phi/2) * sin(phi1/2 + phi2/2)
+    w,x,y,z = qs
+    phi2 = np.arctan2(z, w) - np.arctan2(y, x)
+    PHI = 2 * np.arcsin(np.sqrt(x ** 2 + y ** 2))
+    phi1 = np.arctan2(z, w) + np.arctan2(y, x)
+    return phi2, PHI, phi1
+    
+def normalize(qs):
+    # standardize the quaternion
+    # 1. rotation angle range: [0, pi] -> self.w >= 0
+    # 2. |q| === 1
+    w,x,y,z = qs
+    _sgn = -1 if w < 0 else 1
+    _norm = np.linalg.norm([w, x, y, z]) * _sgn
+    w /= _norm
+    x /= _norm
+    y /= _norm
+    z /= _norm
+    return (w,x,y,z)
+
+def average_quaternions(qs):
+    """
+    Description
+    -----------
+    Return the average quaternion based on algorithm published in
+        F. Landis Markley et.al.
+        Averaging Quaternions,
+        doi: 10.2514/1.28949
+    Parameters
+    ----------
+    qs: quaternions for average
+    
+    Returns
+    -------
+    Quaternion
+        average quaternion of the given list
+    Note:
+    This method only provides an approximation, with about 1% error. 
+    """
+    _sum = np.sum([np.outer(q, q) for q in qs], axis=0)
+    _eigval, _eigvec = np.linalg.eig(_sum / len(qs))
+    return np.real(_eigvec.T[_eigval.argmax()])
+
 # convert euler angles to quaternions
 def rmat_2_quat(rmat):
     r = Rotation.from_matrix(rmat)
@@ -92,21 +161,23 @@ print("Number of Phases present", len(np.unique(np.array(mat_global)))-1)
 for matid in range(matnumber):
     for index in range(len(rotation_matrix1)):
         quats = []
-        
+        rotation_matrix_transformed = []
         for om_ind in range(len(rotation_matrix1[index][0])):
             ##convert the UB matrix to sample reference frame
             orientation_matrix = rotation_matrix1[index][0][om_ind]
-            ## rotate orientation by 40degrees to bring in Sample RF
-            omega = np.deg2rad(-40)
-            cw = np.cos(omega)
-            sw = np.sin(omega)
-            mat_from_lab_to_sample_frame = np.array([[cw, 0.0, sw], [0.0, 1.0, 0.0], [-sw, 0, cw]]) #Y
-            orientation_matrix = np.dot(mat_from_lab_to_sample_frame.T, orientation_matrix)
+            ##Transformation not needed, so we dont have to reapply this to index in Lauetools frame
+            # ## rotate orientation by 40degrees to bring in Sample RF
+            # omega = np.deg2rad(-40)
+            # cw = np.cos(omega)
+            # sw = np.sin(omega)
+            # mat_from_lab_to_sample_frame = np.array([[cw, 0.0, sw], [0.0, 1.0, 0.0], [-sw, 0, cw]]) #Y
+            # orientation_matrix = np.dot(mat_from_lab_to_sample_frame.T, orientation_matrix)
     
-            if np.linalg.det(orientation_matrix) < 0:
-                orientation_matrix = -orientation_matrix
+            # if np.linalg.det(orientation_matrix) < 0:
+            #     orientation_matrix = -orientation_matrix
             #make quaternion from UB matrix
             quats.append(rmat_2_quat(orientation_matrix))
+            rotation_matrix_transformed.append(orientation_matrix)
         quats = np.array(quats)
         
         misori = []
@@ -129,14 +200,41 @@ for matid in range(matnumber):
         bin_index = np.where(zz> 0.05*np.max(zz))[0]
         bin_angles = binzz[bin_index]
         
+        
+        rotation_matrix_transformed = np.array(rotation_matrix_transformed)
+        
+        average_UB = []
         grains = np.copy(misori[ref_index,:])
         for kk, jj in enumerate(bin_angles):
-            cond = (grains<jj+1) * (grains>jj-1)
+            if jj ==0:
+                cond = (grains<jj+1)
+            else:
+                cond = (grains<jj+1) * (grains>jj-1)
+                
+            grain_om = rotation_matrix_transformed[cond, :, :]
+            grain_quats = quats[cond, :]
+            avg_quat_grain = average_quaternions(grain_quats)
+            avg_quat_grain = normalize(avg_quat_grain)
+            avg_euler_grain = quats_as_eulers(avg_quat_grain)
+            avg_om_grain = euler_as_matrix(avg_euler_grain).T
+            average_UB.append(avg_om_grain)
+            ## mask the grain pixels
             grains[cond] = 360 + kk
         
         grains[grains<350] = np.nan
         grains = grains.reshape((lim_x,lim_y))
         grains = grains - 360
+        
+        ####Average UBs misorientation with each other
+        average_UB = np.array(average_UB)
+        quats_UB = []
+        for om_ind in range(len(average_UB)):
+            quats_UB.append(rmat_2_quat(average_UB[om_ind, :, :]))
+        quats_UB = np.array(quats_UB)
+        misori_avg = []
+        for ii in range(len(quats_UB)):
+            misori_avg.append(calc_disorient(quats_UB[ii,:], quats_UB))
+        misori_avg = np.array(misori_avg)
         
         fig = plt.figure(figsize=(11.69,8.27), dpi=100)
         bottom, top = 0.1, 0.9
@@ -152,10 +250,10 @@ for matid in range(matnumber):
         cbar = fig.colorbar(im, cax=cax, orientation='vertical')
         cbar.ax.tick_params(labelsize=8) 
         axs.label_outer()
-        # plt.show()
-        plt.savefig(folder+ "//"+'figure_misorientation_'+str(matid)+"_"+str(index)+'.png', 
-                    bbox_inches='tight',format='png', dpi=1000) 
-        plt.close(fig)
+        plt.show()
+        # plt.savefig(folder+ "//"+'figure_misorientation_'+str(matid)+"_"+str(index)+'.png', 
+        #             bbox_inches='tight',format='png', dpi=1000) 
+        # plt.close(fig)
         
 
 
